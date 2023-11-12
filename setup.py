ff1eef71688a53f0a9f64f114f2a53ee692dc69c
Edwin Török <edwin@etorok.net>
2023-11-12 13:28:14 +0000

    Fix build on Python3.11+
    The code was currently using method 6 [^1], however as the documentation
    warns that will fail if the imported module depends on any packages that
    need to be installed (the build system is about to discover what
    packages need to be installed, so setup.py cannot require them to be
    present!)
    ```
           File "/tmp/pip-build-env-o55qpsst/overlay/lib/python3.12/site-packages/setuptools/build_meta.py", line 507, in run_setup
              super(_BuildMetaLegacyBackend, self).run_setup(setup_script=setup_script)
            File "/tmp/pip-build-env-o55qpsst/overlay/lib/python3.12/site-packages/setuptools/build_meta.py", line 341, in run_setup
              exec(code, locals())
            File "<string>", line 4, in <module>
            File "/var/home/edwin/git/spacy_conll/spacy_conll/__init__.py", line 3, in <module>
              from .formatter import ConllFormatter
            File "/var/home/edwin/git/spacy_conll/spacy_conll/formatter.py", line 5, in <module>
              from spacy.language import Language
          ModuleNotFoundError: No module named 'spacy'
    
    ```
    
    Use method 1, with setup.cfg instead and require setuptools >=  46.4.0
    
    Also add minimal PEP-517 compatibility in `pyproject.toml` [^2].
    
    Tested with: `python3 -m build .`
    
    [1]: https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
    [2]: https://setuptools.pypa.io/en/latest/build_meta.html#how-to-use-it
    
    Signed-off-by: Edwin Török <edwin@etorok.net>

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from setuptools import find_packages, setup
 
-from spacy_conll import __version__
 
 extras = {
     "pd": ["pandas"],
@@ -12,7 +11,6 @@ extras["dev"] = extras["all"] + ["pytest", "flake8", "isort", "black", "pygments
 
 setup(
     name="spacy_conll",
-    version=__version__,
     description="A custom pipeline component for spaCy that can convert any parsed Doc"
                 " and its sentences into CoNLL-U format. Also provides a command line entry point.",
     long_description=Path("README.md").read_text(encoding="utf-8"),
@@ -44,6 +42,9 @@ setup(
     install_requires=[
         "spacy>=3.0.1",
         "dataclasses;python_version<'3.7'"
+    ],
+    setup_requires=[
+      "setuptools>=46.4.0"
     ],
     extras_require=extras,
     entry_points={
